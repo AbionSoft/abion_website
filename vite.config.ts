db411403b7841bd7cec54d58970f9dc87e6666c6
Saw2110 <47711679+Saw2110@users.noreply.github.com>
2026-02-16 02:36:25 +0545

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/abion_website/',  // Must match your repo name exactly
+  base: '/',  // Must match your repo name exactly
 })
